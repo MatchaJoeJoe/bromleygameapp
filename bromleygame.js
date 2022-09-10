a0bd0418e7ba5638bca1362b7887297c31a061aa
Joe <matchajoejoe@gmail.com>
2022-09-10 14:41:30 -0400

    transferring changes from heroku repo

--- bromleygame.js
+++ bromleygame.js
@@ -11,6 +11,7 @@ const io = require('socket.io')(http, {
   }
 });
 var roomsList = [];
+var count = 0;
 
 // root server handling
 app.get('/', (req, res) => {
@@ -19,15 +20,27 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log(`socket ${socket.id} has connected`);
+
   socket.on('list games', (socketId) => {
-    listGames(socket, socketId);
+    joinLFG(socket, socketId);
+  });
+  socket.on('join game', (connectionInfo) =>{
+    joinGame(socket, connectionInfo);
   });
   socket.on('host game', (roomName) => {
     hostGame(socket, roomName);
   });
+  socket.on('player ready', (playerData) => {
+    sendPlayerData(socket, playerData);
+  });
+  socket.on('ship moved', (shipData) => {
+    sendShipData(socket, shipData);
+  });
+  socket.on('disconnect', function () {
+      console.log(`socket ${socket.id} has disconnected`);
+  });
   socket.on('disconnect me', (socketId) => {
     if(socketId == socket.id){
-      console.log(`socket ${socket.id} has disconnected`);
       socket.disconnect();
     }
   });
@@ -54,27 +67,79 @@ io.of("/").adapter.on("leave-room", (room, id) => {
 http.listen(port, function() {
    console.log('listening on *:' + port);
 });
+// function to send player data
+function sendPlayerData(socket, playerData){
+  playerDataObj = JSON.parse(playerData);
+  io.to(playerDataObj.roomName).emit("player-ready", playerData);
 
+}
+function sendShipData(socket, shipData){
+  shipDataObj = JSON.parse(shipData);
+  io.to(shipDataObj.roomName).emit("ship-moved", shipData);
+}
+
+// function to find all socket names
+function findSockets(){
+  connectedSockets = [];
+  var sockets = Array.from(io.sockets.sockets);
+  for (var i=0;i<sockets.length;i++) {
+    thisSocket = sockets[i][0];
+    connectedSockets.push(thisSocket);
+  }
+  return connectedSockets;
+}
+// function to find all room names
+function findRooms() {
+  var clients = findSockets();
+  var availableRooms = [];
+  var rooms =  Array.from(io.sockets.adapter.rooms);
+  for (var i=0;i<rooms.length;i++) {
+    thisRoom = rooms[i][0];
+    if(!clients.includes(thisRoom) && thisRoom != "LFG"){
+      availableRooms.push(thisRoom);
+    }
+  }
+  return availableRooms;
+}
 // function to list available games
 async function listGames(socket, socketId){
+  theRooms = findRooms();
+  openRooms = [];
+  for (var i=0;i<theRooms.length;i++){
+    thisRoom = theRooms[i];
+    var roomSize = io.sockets.adapter.rooms.get(thisRoom).size;
+    if(roomSize == 1){
+      openRooms.push(thisRoom);
+    }
+  }
   var stringList = {
-    strings: roomsList
+    strings: openRooms
   }
   io.to(socketId).emit("games-list", JSON.stringify(stringList));
 }
+// function to host a game
 async function hostGame(socket, roomName){
-  if(roomsList.includes(roomName)){
-    io.to(socket.id).emit("room-created", false);
+  if (io.sockets.adapter.rooms.has(roomName) || roomName == "LFG") {
+    io.to(socket.id).emit("room-created", "false");
   } else {
     socket.join(roomName);
     roomsList.push(roomName);
-    console.log(socket.id + " created " + roomName)
+    io.to(socket.id).emit("room-created", "true");
+    if (io.sockets.adapter.rooms.has("LFG")) {
+      listGames(socket, "LFG");
+    }
   }
-
+}
+// function to join lfg room and list games
+function joinLFG(socket, socketId){
+  socket.join("LFG");
+  listGames(socket, socketId);
 }
 // function to join a game
-async function joinGame(socket, gameId){
-  socket.join(gameId, function () {
-    console.log(socket.id + " now in rooms ", socket.rooms);
-  });
+async function joinGame(socket, connectionInfo){
+  connectionObj = JSON.parse(connectionInfo);
+  socket.leave("LFG");
+  socket.join(connectionObj.gameName);
+  io.to(connectionObj.gameName).emit("player-joined", connectionObj.playerName);
+
 }
